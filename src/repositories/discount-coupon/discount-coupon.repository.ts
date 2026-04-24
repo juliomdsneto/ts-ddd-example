97bffbc253215353cf8d7ae1606e5b1c2b4c493f
juliomachines <julio.mds.neto@gmail.com>
2026-04-24 15:21:01 -0300

    adding discount-coupon entity,mapper,repository

---
+++ src/repositories/discount-coupon/discount-coupon.repository.ts
@@ -0,0 +1,13 @@
+import { Repository } from "../../common/repository/repository.common";
+import { DiscountCouponDoc } from "../../database/docs/discount-coupon.doc";
+import { DiscountCoupon } from "../../entities/discount-coupon.entity";
+import { DiscountCouponMapper } from "./discount-coupon.mapper";
+import { DiscountCoupontModel } from "../../database/models/discount-coupon-model"
+ 
+export class DiscountCouponRepository extends Repository<DiscountCoupon, DiscountCouponDoc>{
+
+    constructor(){
+        super(new DiscountCouponMapper(), new DiscountCoupontModel());
+    }
+
+}
