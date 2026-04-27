d732d4cedd2a8f78df61fa215b376799325edd1d
juliomachines <julio.mds.neto@gmail.com>
2026-04-27 09:21:09 -0300

    adding discount-coupon.service
    also, adding tests inside index.ts

---
+++ src/features/discount-coupon/discount-coupon.service.ts
@@ -0,0 +1,67 @@
+import { Reference } from "../../common/entity/reference.common";
+import { DiscountCouponRepository } from "../../repositories/discount-coupon/discount-coupon.repository"
+import { DiscountCoupon } from "../../entities/discount-coupon.entity"
+import { Entity } from "../../common/entity/entity.common";
+
+export class DiscountCouponService {
+    private discountCouponRepository: DiscountCouponRepository;
+
+    constructor() {
+        this.discountCouponRepository = new DiscountCouponRepository();
+    }
+
+    async create(code: string, value: number) {
+        const discountCoupon = new DiscountCoupon({
+            code: code,
+            value: value
+        });
+
+        const saveCoupon = this.discountCouponRepository.save(discountCoupon);
+        return saveCoupon;
+    }
+
+    async deleteCoupon(id: number) {
+
+        const discountCoupon = this.discountCouponRepository.findById(id);
+
+        if (discountCoupon) {
+
+            return this.discountCouponRepository.delete(discountCoupon);
+        }
+    }
+
+    async updateCoupon(id: number, updateValue: number) {
+        const discountCoupon = this.discountCouponRepository.findById(id);
+
+        if (!discountCoupon) {
+            throw new Error("Can't find Coupon")
+        }
+
+        discountCoupon.value = updateValue;
+
+        const updatedCoupon = this.discountCouponRepository.save(discountCoupon);
+
+        return updatedCoupon;
+
+    }
+
+    async listCoupons(code: string) {
+
+        if (code) {
+
+            const discountCoupon = this.discountCouponRepository.find({ code });
+
+            if (!discountCoupon || discountCoupon.length === 0) {
+                throw new Error("Coupon not found");
+            }
+
+            return discountCoupon
+
+        }
+
+        return this.discountCouponRepository.find({})
+
+    }
+
+
+}
